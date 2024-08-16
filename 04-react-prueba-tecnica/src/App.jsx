dea8ddb8b187a0c837c3b791313a3621f96cc0f8
Sergio <programayaprende422@gmail.com>
2024-08-16 22:59:37 +0200

    4 proyecto prueba tecnica react

---
+++ 04-react-prueba-tecnica/src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+import { useCatImage } from './../hooks/useCatImage'
+import { useCatFact } from './../hooks/useCatFact'
+import { Otro } from '../components/Otro'
+
+export function App () {
+  const { fact, refreshFact } = useCatFact()
+  const { imageUrl } = useCatImage({ fact })
+
+  const handleClick = async () => {
+    refreshFact()
+  }
+
+  return (
+    <main>
+      <h1>App de gatitos</h1>
+      <button onClick={handleClick}>Get new fact</button>
+      {fact && <p>{fact}</p>}
+      {imageUrl && <img src={imageUrl} alt={`Image extracted using the first three words for ${fact}`} />}
+
+      <Otro />
+    </main>
+  )
+}
